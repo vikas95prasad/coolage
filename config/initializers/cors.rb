91ca52e1d086f08aea5bd567f35b80f371c65d6b
Vikas Prasad <vprasad8@apple.com>
2024-07-07 23:01:43 +0530

    Update cros

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Replace with your React app's origin
+    origins 'http://localhost:5173', 'https://coolage-5fbe878e57bb.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
