5b76acc2c779e6e366c6a988ebae5901a23eaa4e
Vikas Prasad <vprasad8@apple.com>
2024-07-08 03:12:33 +0530

    Updated cros file to support urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://coolage-5fbe878e57bb.herokuapp.com'
+    origins 'http://localhost:5173', 'https://coolage-5fbe878e57bb.herokuapp.com', 'https://coolage-web-8ab8e337f4da.herokuapp.com'
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
